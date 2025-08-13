43cce0ee9f275c4d8262a4972907720837922dff
Aguacero 🌧️ <72492241+aguacero7@users.noreply.github.com>
2025-08-13 19:05:18 +0200

    fix format (#19)
    * feat: one shot sync
    
    * feat(sync): minor adjustments on efficiency
    
    * fix: fmt

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -4,4 +4,4 @@
 pub mod sync;
 
 // re-export for a flat public API: rkik::sync::{sync_from_probe, SyncError}
-pub use sync::{sync_from_probe, SyncError};
+pub use sync::{SyncError, sync_from_probe};
